56c944467319753a1772063a408dffc91de42b34
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-16 15:13:31 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  @@pirates = []
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    @@pirates.clear
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  @@ship = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@ship << self
+  end
+
+  def self.all
+    @@ship
+  end
+
+  def self.clear
+    @@ship.clear
+  end
 end
